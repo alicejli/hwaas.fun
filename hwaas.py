7a887a48a95ec2d73fa8495745d32a55a3618899
Alice Li <alicejli@gmail.com>
2020-07-10 18:47:30 +0000

    updating main python file to read from port 80 instead of 5000

--- hwaas.py
+++ hwaas.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route('/hello/<user>')
 def hello_world(user):
     return 'Hello ' + user +'!' +' \n' 
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
